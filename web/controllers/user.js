225701b5b1b1c8c53266ab7cdc4ae90e66c37c3d
Jeremy <jeremy.bankes@gmail.com>
2021-01-27 10:54:40 -0400

    created user view directory

--- web/controllers/user.js
+++ web/controllers/user.js
@@ -1,22 +1,28 @@
 module.exports = (app) => {
 
     app.get('/user', (req, res) => {
-
-        res.render('user', {
+        res.render('user/user', {
             scripts: [
                 '/scripts/chart.js',
                 '/scripts/user/core.js'
             ]
         });
-
     });
 
     app.get('/login', (req, res) => {
-        res.render('login');
+        res.render('user/login');
     });
 
     app.get('/register', (req, res) => {
-        res.render('register');
+        res.render('user/register');
+    });
+
+    app.post('/login', (req, res) => {
+        res.render('todo.hbs');
+    });
+
+    app.post('/user/create', (req, res) => {
+        res.render('todo.hbs');
     });
 
 }
