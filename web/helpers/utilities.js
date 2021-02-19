734fc5a70e35562facc8ade1c2ceb45b0e758a94
kevino29 <varax297@gmail.com>
2021-02-19 16:54:48 -0400

    added a utility helper for brand name

--- web/helpers/utilities.js
+++ web/helpers/utilities.js
@@ -1,8 +1,11 @@
 module.exports = {
+    _brand: function () {
+        return "Uptrend Wealth";
+    },
     upper: function (input) {
         return input.toUpperCase();
     },
     lower: function (input) {
         return input.toLowerCase();
-    }
+    },
 }
