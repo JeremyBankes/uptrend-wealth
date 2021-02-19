86f15601e24d67949f34879bfa15ad470b94aea0
w0441213 <55202158+w0441213@users.noreply.github.com>
2021-02-19 14:28:55 -0400

    Update Capstone_UpTrendWealth.sql

--- data/Capstone_UpTrendWealth.sql
+++ data/Capstone_UpTrendWealth.sql
@@ -1,17 +1,18 @@
-USE master;
-GO
+DROP DATABASE IF EXISTS Uptrend_Wealth;
+CREATE DATABASE Uptrend_Wealth;
 
-DROP DATABASE IF EXISTS UptrendWealth;
-CREATE DATABASE UptrendWealth;
+USE Uptrend_Wealth;
 
+DROP TABLE IF EXISTS Account;
 CREATE TABLE Account (
     AccountID int(11) NOT NULL AUTO_INCREMENT,
     AccountCloseDate timestamp NOT NULL,
     BalanceCALC decimal(19, 0) NOT NULL,
-    UserID int(11),
-    PRIMARY KEY (AccountID)
+    FundOwnershipCALC decimal(19, 0) NOT NULL,
+    UserID int(11), PRIMARY KEY (AccountID)
 );
 
+DROP TABLE IF EXISTS Account_Trade;
 CREATE TABLE Account_Trade (
     AccountAccountID int(11) NOT NULL,
     TradeTradeID int(11) NOT NULL,
@@ -19,24 +20,46 @@ CREATE TABLE Account_Trade (
     PRIMARY KEY (AccountAccountID, TradeTradeID)
 );
 
+DROP TABLE IF EXISTS Address;
+CREATE TABLE Address (
+    AddressID int(11) NOT NULL AUTO_INCREMENT,
+    State varchar(64) NOT NULL,
+    City varchar(64) NOT NULL,
+    Zipcode varchar(10) NOT NULL,
+    UserID int(11) NOT NULL,
+    CountryCountryID int(11),
+    PRIMARY KEY (AddressID)
+);
+
+DROP TABLE IF EXISTS Country;
+CREATE TABLE Country (
+    CountryID int(11) NOT NULL AUTO_INCREMENT,
+    Name varchar(255) NOT NULL,
+    PRIMARY KEY (CountryID)
+);
+
+DROP TABLE IF EXISTS Permission;
 CREATE TABLE Permission (
     PermissionID int(11) NOT NULL AUTO_INCREMENT,
     PermissionName varchar(32) NOT NULL,
     PRIMARY KEY (PermissionID)
 );
 
+DROP TABLE IF EXISTS Role;
 CREATE TABLE Role (
     RoleID int(11) NOT NULL AUTO_INCREMENT,
     RoleName varchar(32) NOT NULL,
     PRIMARY KEY (RoleID)
 );
 
+DROP TABLE IF EXISTS Role_Permission;
 CREATE TABLE Role_Permission (
     RoleRoleID int(11) NOT NULL,
     PermissionPermissionID int(11) NOT NULL,
     PRIMARY KEY (RoleRoleID, PermissionPermissionID)
 );
 
+DROP TABLE IF EXISTS Trade;
 CREATE TABLE Trade (
     TradeID int(11) NOT NULL AUTO_INCREMENT,
     CurrencyPair varchar(6) NOT NULL,
@@ -53,6 +76,7 @@ CREATE TABLE Trade (
     PRIMARY KEY (TradeID)
 );
 
+DROP TABLE IF EXISTS Transfer;
 CREATE TABLE Transfer (
     TransferID int(11) NOT NULL AUTO_INCREMENT,
     Amount decimal(19, 0) NOT NULL,
@@ -61,17 +85,21 @@ CREATE TABLE Transfer (
     PRIMARY KEY (TransferID)
 );
 
+DROP TABLE IF EXISTS `User`;
 CREATE TABLE `User` (
     UserID int(11) NOT NULL AUTO_INCREMENT,
     FirstName varchar(64) NOT NULL,
     LastName varchar(64) NOT NULL,
     Address varchar(255) NOT NULL,
-    PhoneNumber varchar(32) NOT NULL,
+    PhoneNumber varchar(64) NOT NULL,
+    EmailAddress varchar(255) NOT NULL,
     RegistrationDate timestamp NOT NULL,
-    UnregistrationDate timestamp NULL,
+    UnregistrationDate timestamp NOT NULL,
+    Hash blob,
     PRIMARY KEY (UserID)
 );
 
+DROP TABLE IF EXISTS User_Role;
 CREATE TABLE User_Role (
     UserUserID int(11) NOT NULL,
     RoleRoleID int(11) NOT NULL,
@@ -79,12 +107,43 @@ CREATE TABLE User_Role (
 );
 
 ALTER TABLE Account
-    ADD CONSTRAINT FKAccount621444
-    FOREIGN KEY (UserID) REFERENCES `User` (UserID);
-ALTER TABLE User_Role ADD CONSTRAINT FKUser_Role448688 FOREIGN KEY (UserUserID) REFERENCES `User` (UserID);
-ALTER TABLE User_Role ADD CONSTRAINT FKUser_Role764707 FOREIGN KEY (RoleRoleID) REFERENCES Role (RoleID);
-ALTER TABLE Role_Permission ADD CONSTRAINT FKRole_Permi338895 FOREIGN KEY (RoleRoleID) REFERENCES Role (RoleID);
-ALTER TABLE Role_Permission ADD CONSTRAINT FKRole_Permi603637 FOREIGN KEY (PermissionPermissionID) REFERENCES Permission (PermissionID);
-ALTER TABLE Transfer ADD CONSTRAINT FKTransfer763027 FOREIGN KEY (AccountAccountID) REFERENCES Account (AccountID);
-ALTER TABLE Account_Trade ADD CONSTRAINT FKAccount_Tr358925 FOREIGN KEY (AccountAccountID) REFERENCES Account (AccountID);
-ALTER TABLE Account_Trade ADD CONSTRAINT FKAccount_Tr801316 FOREIGN KEY (TradeTradeID) REFERENCES Trade (TradeID);
+    ADD CONSTRAINT FK_Account_UserID
+        FOREIGN KEY (UserID)
+            REFERENCES `User` (UserID);
+ALTER TABLE User_Role
+    ADD CONSTRAINT FK_User_Role_UserUserID
+        FOREIGN KEY (UserUserID)
+            REFERENCES `User` (UserID);
+ALTER TABLE User_Role
+    ADD CONSTRAINT FK_User_Role_RoleRoleID
+        FOREIGN KEY (RoleRoleID)
+            REFERENCES Role (RoleID);
+ALTER TABLE Role_Permission
+    ADD CONSTRAINT FK_Role_Permission_RoleRoleID
+        FOREIGN KEY (RoleRoleID)
+            REFERENCES Role (RoleID);
+ALTER TABLE Role_Permission
+    ADD CONSTRAINT FK_Role_Permission_PermissionPermissionID
+        FOREIGN KEY (PermissionPermissionID)
+            REFERENCES Permission (PermissionID);
+ALTER TABLE Transfer
+    ADD CONSTRAINT FK_Transfer_AccountAccountID
+        FOREIGN KEY (AccountAccountID)
+            REFERENCES Account (AccountID);
+ALTER TABLE Account_Trade
+    ADD CONSTRAINT FK_Account_Trade_AccountAccountID
+        FOREIGN KEY (AccountAccountID)
+            REFERENCES Account (AccountID);
+ALTER TABLE Account_Trade
+    ADD CONSTRAINT FK_Account_Trade_TradeTradeID
+        FOREIGN KEY (TradeTradeID)
+            REFERENCES Trade (TradeID);
+ALTER TABLE Address
+    ADD CONSTRAINT FK_Address_CountryCountryID
+        FOREIGN KEY (CountryCountryID)
+            REFERENCES Country (CountryID);
+ALTER TABLE Address
+    ADD CONSTRAINT FK_Address_UserID
+        FOREIGN KEY (UserID)
+            REFERENCES `User` (UserID);
+
