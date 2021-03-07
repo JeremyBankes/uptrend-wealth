c85f13867052329fb86619597d67c52dcfce70f9
kevino29 <varax297@gmail.com>
2021-03-06 21:49:15 -0400

    added a starter code for charts

--- web/public/js/google_charts.js
+++ web/public/js/google_charts.js
@@ -3,26 +3,64 @@ google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
     // GET THE DATA
-    var data = google.visualization.arrayToDataTable([
+    // USE ARRAY OR DATATABLE
+    const data = google.visualization.arrayToDataTable([
         ['Year', 'Sales'],
-        ['2004',  1000],
-        ['2005',  1170],
-        ['2006',  660],
-        ['2007',  1030]
+        ['2004',  2000],
+        ['2005',  4000],
+        ['2006',  10000],
+        ['2007',  3500],
+    ]);
+
+    const dataTable = new google.visualization.DataTable();
+    dataTable.addColumn({type: 'string', 'id': 'Year'});
+    dataTable.addColumn({type: 'number', 'id': 'Sales'});
+    dataTable.addRows([
+        ['2004', 2000],
+        ['2005', 4000],
+        ['2006', 10000],
+        ['2007', 3500],
     ]);
 
     // LEARN THE OPTIONS
     // https://developers.google.com/chart/interactive/docs/gallery/linechart#configuration-options
     var options = {
-        title: 'Company Performance',
         curveType: 'none',
-        vAxis: { title: 'Balance', titleTextStyle: { color: '#660000' } },
-        hAxis: { title: 'Date', titleTextStyle: { color: '#660000' } },
-        height: '430',
-        width: '710',
+        chartArea : {
+            width: '90%',
+            height: '90%',
+        },
+        animation: {
+            startup: true,
+            easing: 'in',
+            duration: 3000,
+        },
+        legend: {
+            position: 'none',
+        },
+        explorer: {
+            axis: 'horizontal',
+        },
+        vAxis: { 
+            title: 'Balance', 
+            titleTextStyle: { 
+                color: '#660000',
+                italic: false,
+            },
+            format: 'currency',
+        },
+        hAxis: {
+            title: 'Date', 
+            titleTextStyle: { 
+                color: '#660000',
+                italic: false,
+            },
+        },
+        height: 550,
+        width: 900,
     };
 
     // CREATE AND DRAW THE CHART
     var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-    chart.draw(data, options);
+    chart.draw(dataTable, options);
 }
