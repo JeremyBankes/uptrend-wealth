cc82dbb4ccd81e79727ebc360435f43d72f04059
Kevin Orenday <varax297@gmail.com>
2021-03-27 23:37:06 -0300

    transitioned from bootstrap 4 to mdb 5

--- web/public/js/scrollbar_animation.js
+++ web/public/js/scrollbar_animation.js
@@ -6,5 +6,10 @@ window.addEventListener('load', function() {
         let bodyHeight = document.body.clientHeight - window.innerHeight;
         let newWidth = (currHeight/bodyHeight) * 100;
         scrollbar.style.width = newWidth + '%';
+
+        if (scrollbar.style.width === '0%')
+            scrollbar.classList.add('d-none');
+        else
+            scrollbar.classList.remove('d-none');
     });
 });
