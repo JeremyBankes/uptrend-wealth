65193fcd7fff4fbbea18cf21480d0690b0f5d57e
Kevin Orenday <varax297@gmail.com>
2021-03-28 12:03:04 -0300

    created a library of scripts

---
+++ web/public/js/lib.js
@@ -0,0 +1,19 @@
+window.addEventListener('load', async () => {
+    await scrollbarAnimation();
+});
+
+async function scrollbarAnimation() {
+    let scrollbar = document.querySelector('.scrollbar-progress');
+
+    window.addEventListener('scroll', function(e) {
+        let currHeight = window.pageYOffset;
+        let bodyHeight = document.body.clientHeight - window.innerHeight;
+        let newWidth = (currHeight/bodyHeight) * 100;
+        scrollbar.style.width = newWidth + '%';
+
+        if (scrollbar.style.width === '0%')
+            scrollbar.classList.add('d-none');
+        else
+            scrollbar.classList.remove('d-none');
+    });
+}
